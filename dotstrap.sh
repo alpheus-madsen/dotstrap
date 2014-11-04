54177d05b9b9bbfa5d3d6701aa2db4745577efb1
Alpheus Madsen <alpheus.madsen@gmail.com>
2014-11-03 17:14:10 -0700

    First Commit -- Initial Filestructure
    Created the "dotstrap.sh" Dotfile Bootstrap script, which backs up
    default settings and then symlinks settings to mine.
    
    Also created (mostly empty) settings files, in anticipation of filling
    these things out to my customizing delight!
    w

---
+++ dotstrap.sh
@@ -0,0 +1,70 @@
+### dotstrap.sh
+###
+### Preserve default dotfiles and create symlinks to prefered dotfiles.
+### This file is created using the following website as a template:
+###
+###    www.placona.com.uk/1224/linux/managing-your-dotfiles-the-right-way/
+
+
+###### Variables
+
+dir=~/.dotfiles                      # dotfiles directory
+olddir=~/.dotfiles/default-dotfiles  # old dotfiles backup directory
+
+# list of files and folders to symlink in homedir
+files=(
+    ### bash config files
+    .bashrc
+    # .local.bash
+    # .bash_profile
+    # .bash_login
+    # .bash_logout
+    # .bash/
+
+    ### Vim config files
+    .vimrc
+    .vim/
+
+    ### Emacs config files
+    #.emacs
+    #.emacs.el
+    .emacs.d/
+
+    ### Screen, tmux, git, ssh
+    .screenrc
+    .tmux.conf
+    .gitconfig
+    .ssh/
+
+    ### Zsh -- I'm not quite sure if I'll be taking the plunge!
+    # .zshrc
+    # .zshenv
+    # .zprofile
+    # .zlogin
+    # .zsh/
+)
+
+######
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+echo
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in ${files[*]}; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/$file $olddir/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+    echo
+done
+
+# execute any files to reflect preferences...
+source ~/.bashrc
+source ~/.vimrc
